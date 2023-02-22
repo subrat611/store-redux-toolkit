68f24ea5996ffa484308f1e0f764c6a7fffd57d0
subrat <subratj611@gmail.com>
2023-02-22 22:40:24 +0530

    navbar, routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import HomePage from "./pages/HomePage";
+
 function App() {
-  return <div className="App">code here</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<NavBar />}>
+        <Route index element={<HomePage />} />
+        <Route path="shop" element={<h1>shop</h1>} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,28 @@
+import { Link, Outlet } from "react-router-dom";
+
+import { ReactComponent as ShoppingCart } from "../../assets/shopping-cart.svg";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+
+export default function NavBar() {
+  return (
+    <div className="wrapper">
+      <nav className="navbar-wrapper">
+        <span className="nav-logo">
+          <Logo />
+        </span>
+        <ul className="nav-lists">
+          <li className="nav-list">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav-list">
+            <Link to="/shop">Shop</Link>
+          </li>
+        </ul>
+        <span className="nav-cart">
+          <ShoppingCart />
+        </span>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
